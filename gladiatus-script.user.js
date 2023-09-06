2cff672577edb30963801c8e299716b7bc8aa697
Ensei Tankado <jozef.karlik@protonmail.com>
2023-09-07 00:16:24 +0200

    Edit source

--- gladiatus-script.user.js
+++ gladiatus-script.user.js
@@ -5,12 +5,12 @@
 // @author       Eryk Bodziony
 // @match        *://*.gladiatus.gameforge.com/game/index.php*
 // @exclude      *://*.gladiatus.gameforge.com/game/index.php?mod=start
-// @downloadURL  https://github.com/ebodziony/gladiatus-script/raw/master/gladiatus-script.js
-// @updateURL    https://github.com/ebodziony/gladiatus-script/raw/master/gladiatus-script.js
+// @downloadURL  https://github.com/etaoin/g-scripts/raw/master/gladiatus-script.js
+// @updateURL    https://github.com/etaoin/g-scripts/raw/master/gladiatus-script.js
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
-// @resource     customCSS_global  https://raw.githubusercontent.com/ebodziony/gladiatus-script/master/global.css?ver=2.6
+// @resource     customCSS_global  https://raw.githubusercontent.com/etaoin/g-scripts/master/global.css?ver=2.6
 // ==/UserScript==
 
 
